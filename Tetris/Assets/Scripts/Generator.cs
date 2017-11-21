fe835a42f9c682a799159c922b077433fdd2a013
mysterybiscuit <guest91111@yandex.com>
2017-11-21 20:46:49 +0100

    Iniital Project
    First time opening Unity, these are the pre-generated files. Also added a block generator.

---
+++ Tetris/Assets/Scripts/Generator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour {
+
+    public List<GameObject> tetrominos = new List<GameObject>();
+    
+    // Use this for initialization
+	void Start () {
+        generateNextTetromino();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void generateNextTetromino()
+    {
+        int random = Random.Range(0, tetrominos.Count);
+
+        Instantiate(tetrominos[random], transform.position, Quaternion.identity);
+        //Generates tetromino at the spawn position with default rotation.
+
+    }
+}
