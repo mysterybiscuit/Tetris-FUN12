e8c2ca2a9625ce47f9c3189ef4614798ae4c84a1
mysterybiscuit <guest91111@yandex.com>
2017-12-14 14:11:05 +0100

    Started fixing Tetris
    Made some changes to start fixing tetris.

--- Tetris/Assets/Scripts/Grid.cs
+++ Tetris/Assets/Scripts/Grid.cs
@@ -8,70 +8,70 @@ public class Grid : MonoBehaviour {
     public static int height = 20;
     public static Transform[,] grid = new Transform[width, height];
 
-    public static Vector2 roundOff(Vector2 vector)
-    {
-        return new Vector2(Mathf.Round(vector.x), Mathf.Round(vector.y));
-    }
+    //public static Vector2 roundOff(Vector2 vector)
+    //{
+    //    return new Vector2(Mathf.Round(vector.x), Mathf.Round(vector.y));
+    //}
 
-    public static bool inGrid(Vector2 pos)
-    {
-        return ((int)pos.x >= 0 && (int)pos.x < width && (int)pos.y >= 0);
-    }
+    //public static bool inGrid(Vector2 pos)
+    //{
+    //    return ((int)pos.x >= 0 && (int)pos.x < width && (int)pos.y >= 0);
+    //}
 
-    public static void removeRow(int y)
-    {
-        for (int x = 0; x < width; ++x)
-        {
-            Destroy(grid[x, y].gameObject);
-            grid[x, y] = null;
-        }
-    }
+    //public static void removeRow(int y)
+    //{
+    //    for (int x = 0; x < width; ++x)
+    //    {
+    //        Destroy(grid[x, y].gameObject);
+    //        grid[x, y] = null;
+    //    }
+    //}
 
-    public static void lowerRow(int y)
-    {
-        for (int x = 0; x < width; ++x)
-        {
-            if (grid[x,y] = null)
-            {
-                grid[x, y - 1] = grid[x, y];
-                grid[x, y] = null;
+    //public static void lowerRow(int y)
+    //{
+    //    for (int x = 0; x < width; ++x)
+    //    {
+    //        if (grid[x,y] = null)
+    //        {
+    //            grid[x, y - 1] = grid[x, y];
+    //            grid[x, y] = null;
 
-                grid[x, y - 1].position += new Vector3(0, -1, 0);
-            }
-        }
-    }
+    //            grid[x, y - 1].position += new Vector3(0, -1, 0);
+    //        }
+    //    }
+    //}
     
-    public static void repeatLowerRow(int y)
-    {
-        for (int i = y; i < height; ++i)
-        {
-            lowerRow(i);
-        }
-    }
+    //public static void repeatLowerRow(int y)
+    //{
+    //    for (int i = y; i < height; ++i)
+    //    {
+    //        lowerRow(i);
+    //    }
+    //}
 
-    public static bool checkFullRow(int y)
-    {
-        for (int x = 0; x < width; ++x)
-        {
-            if (grid[x,y] == null)
-            {
-                return false;
-            }
+    //public static bool checkFullRow(int y)
+    //{
+    //    for (int x = 0; x < width; ++x)
+    //    {
+    //        if (grid[x,y] == null)
+    //        {
+    //            return false;
+    //        }
            
-        }
-        return true;
-    }
+    //    }
+    //    return true;
+    //}
 
-    public static void removeFullRows()
-    {
-        for (int y = 0; y < height; ++y)
-        {
-            if (checkFullRow(y))
-            {
-                removeRow(y);
-                repeatLowerRow(y + 1);
-                --y;
-            }
-        }
-    }
+    //public static void removeFullRows()
+    //{
+    //    for (int y = 0; y < height; ++y)
+    //    {
+    //        if (checkFullRow(y))
+    //        {
+    //            removeRow(y);
+    //            repeatLowerRow(y + 1);
+    //            --y;
+    //        }
+    //    }
+    //}
 }
--- Tetris/Assets/Scripts/Group.cs
+++ Tetris/Assets/Scripts/Group.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Group : MonoBehaviour {
+public class Group : MonoBehaviour
+{
 
     public float blockSize = 0.65f;
 
@@ -20,73 +21,89 @@ public class Group : MonoBehaviour {
 
     void Start()
     {
-        if (!checkForValidGridPosition())
-        {
-            Debug.Log(gameOverString);
-            Destroy(gameObject);
-        }
-    }
+        //    if (!checkForValidGridPosition())
+        //    {
+        //        Debug.Log(gameOverString);
+        //        Destroy(gameObject);
+        //    }
+        //}
 
-    bool checkForValidGridPosition()
-    {
-        foreach (Transform block in transform)
-        {
-            Vector2 vector = Grid.roundOff(block.position);
-            if (!Grid.inGrid(vector))
-            {
-                return false;
-            }
+        //bool checkForValidGridPosition()
+        //{
+        //    foreach (Transform block in transform)
+        //    {
+        //        Vector2 vector = Grid.roundOff(block.position);
+        //        if (!Grid.inGrid(vector))
+        //        {
+        //            return false;
+        //        }
 
-            if (Grid.grid[(int)vector.x, (int)vector.y] != null && Grid.grid[(int)vector.x, (int)vector.y].parent != transform)
-            {
-                return false;
-            } 
-        }
-        return true;
+        //        if (Grid.grid[(int)vector.x, (int)vector.y] != null && Grid.grid[(int)vector.x, (int)vector.y].parent != transform)
+        //        {
+        //            return false;
+        //        } 
+        //    }
+        //    return true;
     }
 
     void drawGrid()
     {
-        for (int y = 0; y < Grid.height; ++y)
-        {
-            for (int x = 0; x < Grid.width; ++x)
-            {
-                if (Grid.grid[x, y] != null)
-                {
-                    if (Grid.grid[x, y].parent == transform)
-                    {
-                        Grid.grid[x, y] = null;
-                    }
-                    
-                }
-            }
-        }
+        //for (int y = 0; y < Grid.height; ++y)
+        //{
+        //    for (int x = 0; x < Grid.width; ++x)
+        //    {
+        //        if (Grid.grid[x, y] != null)
+        //        {
+        //            if (Grid.grid[x, y].parent == transform)
+        //            {
+        //                Grid.grid[x, y] = null;
+        //            }
 
-        foreach (Transform block in transform)
-        {
+        //        }
+        //    }
+        //}
 
-            foreach (GameObject erase in toDestroy)
-            {
-                Destroy(erase);
-            }
-            toDestroy = GameObject.FindGameObjectsWithTag("test");
-            Vector2 vector = Grid.roundOff(block.position);
-            Grid.grid[(int)vector.x, (int)vector.y] = block;
-            Instantiate(test, block.position, Quaternion.identity);
+        //foreach (Transform block in transform)
+        //{
 
-        }
+        //    foreach (GameObject erase in toDestroy)
+        //    {
+        //        Destroy(erase);
+        //    }
+        //    toDestroy = GameObject.FindGameObjectsWithTag("test");
+        //    Vector2 vector = Grid.roundOff(block.position);
+        //    Grid.grid[(int)vector.x, (int)vector.y] = block;
+        //    Instantiate(test, block.position, Quaternion.identity);
+
+        //}
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    private bool inGrid() //Checks for each child of a tetromino whether it is still in the grid
+    {
+        foreach (Transform child in transform)
+        {
+            Vector2 pos = child.position;
+            if (!inBorder(pos))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static bool inBorder(Vector2 posGroup) //Checks whether block is in grid
+    {
+        return ((float)posGroup.x >= -3.6 && (float)posGroup.x <=3.6 && (float)posGroup.y >= -4.2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
         if (Input.GetKeyDown(KeyCode.A))
         {
             transform.position += new Vector3(-blockSize, 0, 0);
-            if (checkForValidGridPosition())
-            {
-                drawGrid();
-            }
-            else
+            if (!inGrid())
             {
                 transform.position += new Vector3(blockSize, 0, 0);
             }
@@ -94,11 +111,7 @@ public class Group : MonoBehaviour {
         else if (Input.GetKeyDown(KeyCode.D))
         {
             transform.position += new Vector3(blockSize, 0, 0);
-            if (checkForValidGridPosition())
-            {
-                drawGrid();
-            }
-            else
+            if (!inGrid())
             {
                 transform.position += new Vector3(-blockSize, 0, 0);
             }
@@ -106,24 +119,15 @@ public class Group : MonoBehaviour {
         else if (Input.GetKeyDown(KeyCode.O))
         {
             transform.Rotate(0, 0, -90);
-            if (checkForValidGridPosition())
-            {
-                drawGrid();
-            }
-            else
+            if (!inGrid())
             {
                 transform.Rotate(0, 0, 90);
             }
-
         }
         else if (Input.GetKeyDown(KeyCode.P))
         {
             transform.Rotate(0, 0, 90);
-            if (checkForValidGridPosition())
-            {
-                drawGrid();
-            }
-            else
+            if (!inGrid())
             {
                 transform.Rotate(0, 0, -90);
             }
@@ -131,20 +135,79 @@ public class Group : MonoBehaviour {
         else if (Input.GetKeyDown(KeyCode.S) || Time.time - lastDrop >= dropTime)
         {
             transform.position += new Vector3(0, -blockSize, 0);
-            if (checkForValidGridPosition())
-            {
-                drawGrid();
-            }
-            else
+            if (!inGrid())
             {
                 transform.position += new Vector3(0, blockSize, 0);
-                Grid.removeFullRows();
-                GameObject.FindObjectOfType<Generator>().generateNextTetromino();
-                enabled = false;
             }
             lastDrop = Time.time;
         }
-	}
+        //       if (Input.GetKeyDown(KeyCode.A))
+        //       {
+        //           transform.position += new Vector3(-blockSize, 0, 0);
+        //           if (checkForValidGridPosition())
+        //           {
+        //               drawGrid();
+        //           }
+        //           else
+        //           {
+        //               transform.position += new Vector3(blockSize, 0, 0);
+        //           }
+        //       }
+        //       else if (Input.GetKeyDown(KeyCode.D))
+        //       {
+        //           transform.position += new Vector3(blockSize, 0, 0);
+        //           if (checkForValidGridPosition())
+        //           {
+        //               drawGrid();
+        //           }
+        //           else
+        //           {
+        //               transform.position += new Vector3(-blockSize, 0, 0);
+        //           }
+        //       }
+        //       else if (Input.GetKeyDown(KeyCode.O))
+        //       {
+        //           transform.Rotate(0, 0, -90);
+        //           if (checkForValidGridPosition())
+        //           {
+        //               drawGrid();
+        //           }
+        //           else
+        //           {
+        //               transform.Rotate(0, 0, 90);
+        //           }
 
-    
+        //       }
+        //       else if (Input.GetKeyDown(KeyCode.P))
+        //       {
+        //           transform.Rotate(0, 0, 90);
+        //           if (checkForValidGridPosition())
+        //           {
+        //               drawGrid();
+        //           }
+        //           else
+        //           {
+        //               transform.Rotate(0, 0, -90);
+        //           }
+        //       }
+        //       else if (Input.GetKeyDown(KeyCode.S) || Time.time - lastDrop >= dropTime)
+        //       {
+        //           transform.position += new Vector3(0, -blockSize, 0);
+        //           if (checkForValidGridPosition())
+        //           {
+        //               drawGrid();
+        //           }
+        //           else
+        //           {
+        //               transform.position += new Vector3(0, blockSize, 0);
+        //               Grid.removeFullRows();
+        //               GameObject.FindObjectOfType<Generator>().generateNextTetromino();
+        //               enabled = false;
+        //           }
+        //           lastDrop = Time.time;
+        //       }
+        //}
+
+
+    }
 }
