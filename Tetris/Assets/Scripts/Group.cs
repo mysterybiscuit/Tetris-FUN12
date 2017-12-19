1c46ab8e0d7bc0e570ef7a3e3509033ebcbaf2ea
mysterybiscuit <guest91111@yandex.com>
2017-12-19 08:46:10 +0100

    Push

--- Tetris/Assets/Scripts/Group.cs
+++ Tetris/Assets/Scripts/Group.cs
@@ -6,13 +6,11 @@ using Grid = UnityEngine.Grid;
 public class Group : MonoBehaviour
 {
 
-    public float blockSize = 0.65f;
-
-    float mostRecentFall = 0f;
+    public float blockSize = 0.325f;
 
     float lastDrop = 0f;
 
-    public float dropTime = 1000000f;
+    public float dropTime = 0.75f;
 
     public string gameOverString = "Game over!";
 
@@ -21,6 +19,75 @@ public class Group : MonoBehaviour
     public GameObject[] toDestroy;
 
     private int posx, posy;
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-blockSize, 0, 0);
+            if (!inGrid())
+            {
+                transform.position += new Vector3(blockSize, 0, 0);
+            }
+            else
+            {
+                updateGrid();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(blockSize, 0, 0);
+            if (!inGrid())
+            {
+                transform.position += new Vector3(-blockSize, 0, 0);
+            }
+            else
+            {
+                updateGrid();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            transform.Rotate(0, 0, -90);
+            if (!inGrid())
+            {
+                transform.Rotate(0, 0, 90);
+            }
+            else
+            {
+                updateGrid();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            transform.Rotate(0, 0, 90);
+            if (!inGrid())
+            {
+                transform.Rotate(0, 0, -90);
+            }
+            else
+            {
+                updateGrid();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Time.time - lastDrop >= dropTime)
+        {
+            transform.position += new Vector3(0, -blockSize, 0);
+            if (!inGrid())
+            {
+                transform.position += new Vector3(0, blockSize, 0);
+                enabled = false;
+                FindObjectOfType<Generator>().generateNextTetromino();
+            }
+            else
+            {
+                updateGrid();
+            }
+            lastDrop = Time.time;
+        }
+
+    }
 
     private bool inGrid() //Checks for each child of a tetromino whether it is still in the grid
     {
@@ -35,10 +102,8 @@ public class Group : MonoBehaviour
             //Formula for position (-1.85 -> 1.85, 4 -> -3.8)
             //x: pos = (10/3.7)x + (10 - 18.5/3.7)
             //y: pos = -(80/7.8)x + (20 - 80/7.8)
-            //TODO: FIX FORMULA OF Y
-            int posx = (int)((10 / 3.7) * pos.x + (10 - 18.5 / 3.7));
-            int posy = (int)((-7.8 / 80) * pos.y + (20 - 80 / 7.8));
-            Debug.Log(posx + ", " + posy);
+            posx = (int)((10 / 3.7) * pos.x + (10 - 18.5 / 3.7));
+            posy = (int)((-7.8 / 80) * pos.y + (20 - 80 / 7.8));
 
             if (GridClass.grid[posx, posy] != null && GridClass.grid[posx, posy].parent != transform)
             {
@@ -54,75 +119,7 @@ public class Group : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            transform.position += new Vector3(-blockSize, 0, 0);
-            if (!inGrid())
-            {
-                transform.position += new Vector3(blockSize, 0, 0);
-            }
-            else
-            {
-                //updateGrid();
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            transform.position += new Vector3(blockSize, 0, 0);
-            if (!inGrid())
-            {
-                transform.position += new Vector3(-blockSize, 0, 0);
-            }
-            else
-            {
-                //updateGrid();
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.O))
-        {
-            transform.Rotate(0, 0, -90);
-            if (!inGrid())
-            {
-                transform.Rotate(0, 0, 90);
-            }
-            else
-            {
-                //updateGrid();
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.P))
-        {
-            transform.Rotate(0, 0, 90);
-            if (!inGrid())
-            {
-                transform.Rotate(0, 0, -90);
-            }
-            else
-            {
-                //updateGrid();
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.S) || Time.time - lastDrop >= dropTime)
-        {
-            transform.position += new Vector3(0, -blockSize, 0);
-            if (!inGrid())
-            {
-                transform.position += new Vector3(0, blockSize, 0);
-                enabled = false;
-                FindObjectOfType<Generator>().generateNextTetromino();
-            }
-            else
-            {
-                //updateGrid();
-            }
-            lastDrop = Time.time;
-            Debug.Log("Dropped!");
-        }
-
-    }
+    
 
     private void updateGrid()
     {
@@ -139,11 +136,8 @@ public class Group : MonoBehaviour
 
         foreach (Transform child in transform)
         {
-            Vector2 pos = child.position;
-            if (pos.y < 5)
-            {
-                GridClass.grid[posx, posy] = child;
-            }
+            GridClass.grid[posx, posy] = child;
         }
+        GridClass.printArray();
     }
 }
